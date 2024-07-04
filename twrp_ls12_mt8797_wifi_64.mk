2408d44b1efdf4703a3fed39270ce9177b4c34e2
wt_bruhhhh <59949135+ZH-XiJun@users.noreply.github.com>
2024-07-04 22:12:28 +0800

    Update and rename omni_ls12_mt8797_wifi_64.mk to twrp_ls12_mt8797_wifi_64.mk

--- twrp_ls12_mt8797_wifi_64.mk
+++ twrp_ls12_mt8797_wifi_64.mk
@@ -10,13 +10,13 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, vendor/twrp/config/common.mk)
 
 # Inherit from ls12_mt8797_wifi_64 device
 $(call inherit-product, device/alps/ls12_mt8797_wifi_64/device.mk)
 
 PRODUCT_DEVICE := ls12_mt8797_wifi_64
-PRODUCT_NAME := omni_ls12_mt8797_wifi_64
+PRODUCT_NAME := twrp_ls12_mt8797_wifi_64
 PRODUCT_BRAND := alps
 PRODUCT_MODEL := TALIH-PD2
 PRODUCT_MANUFACTURER := alps
